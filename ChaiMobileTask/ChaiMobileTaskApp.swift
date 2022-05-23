50dc3e7c4f186bb7696ce890760fc0c41eee66ab
Steve Park <seongwuk.park@gmail.com>
2022-05-23 18:45:10 +1200

    Initial Commit

---
+++ ChaiMobileTask/ChaiMobileTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaiMobileTaskApp.swift
+//  ChaiMobileTask
+//
+//  Created by Seongwuk Park on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ChaiMobileTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaiMobileTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChaiMobileTask
+//
+//  Created by Seongwuk Park on 23/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
